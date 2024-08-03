920e6d99c6e62abd4fd35a43b8640d650bb1c996
ugbeadie <ugbeadie3@gmail.com>
2024-08-03 18:53:08 +0100

    client and server rendering

--- app/page.js
+++ app/page.js
@@ -1,7 +1,47 @@
-import React from "react";
+// "use client";
 
-const Home = () => {
-  return <div>Home</div>;
+// import { useEffect, useState } from "react";
+
+// const Data = () => {
+//   const [product, setProduct] = useState([]);
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       let data = await fetch("https://jsonplaceholder.typicode.com/posts");
+//       data = await data.json();
+//       console.log(data);
+//       setProduct(data);
+//     };
+//     fetchData();
+//   });
+
+//   return (
+//     <div>
+//       {product.map((p) => (
+//         <p key={p.id}>{p.title}</p>
+//       ))}
+//     </div>
+//   );
+// };
+
+// export default Data;
+
+const fetchData = async () => {
+  let data = await fetch("https://jsonplaceholder.typicode.com/posts");
+  data = await data.json();
+  return data;
 };
 
-export default Home;
+const Data = async () => {
+  let products = await fetchData();
+  console.log(products);
+
+  return (
+    <div>
+      {products.map((p) => (
+        <p key={p.id}>{p.title}</p>
+      ))}
+    </div>
+  );
+};
+
+export default Data;
